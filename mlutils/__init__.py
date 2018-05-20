408bcb6e018eb33cfe4bc354fba0b3902434a750
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-05-20 21:24:45 +0530

    Package initialization

---
+++ mlutils/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Package initialization.
+Created on Sun May 20 20:00:00 2018
+Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
+GitHub: https://github.com/prasunroy/mlutils
+
+"""
+
+
+__version__ = '0.1.0'
