6441ffbdf91e046cfde0fdc78c08e3b2060af200
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-06-03 19:24:14 +0530

    Updated module

--- mlutils/callbacks.py
+++ mlutils/callbacks.py
@@ -9,46 +9,71 @@ GitHub: https://github.com/prasunroy/mlutils
 
 
 # imports
+import os
 import requests
 
 from keras import callbacks
+from matplotlib import pyplot
 from random import randint
 
 
 # Telegram class
 class Telegram(callbacks.Callback):
-    def __init__(self, auth_token, chat_id, monitor='val_acc'):
-        self._id = None
-        self._url = 'https://api.telegram.org/bot{}'.format(auth_token)
-        self._data = {'chat_id': chat_id}
-        self._files = {}
-        self._record = {}
-        self._monitor = {monitor: None}
-        
+    
+    def __init__(self, auth_token, chat_id, monitor='val_acc', out_dir='.'):
+        self._task_id = None
+        self._chat_id = chat_id
+        self._address = 'https://api.telegram.org/bot{}'.format(auth_token)
+        self._history = {}
+        self._monitor = {monitor.lower(): None}
+        self._out_dir = out_dir
         return
     
     def on_train_begin(self, logs):
+        self._task_id = randint(1000, 9999)
         for metric in self.params.get('metrics'):
-            self._record[metric] = []
-        self._id = randint(1000, 9999)
-        self._data['text'] = '`A new training has been started [ID: {}].`'.format(self._id)
-        self._data['parse_mode'] = 'Markdown'
-        self._send_message()
-        
+            self._history[metric] = []
+        data = {}
+        data['chat_id'] = self._chat_id
+        data['text'] = '`A new training has been started.\n[TASK ID: {}]`'.format(self._task_id)
+        data['parse_mode'] = 'Markdown'
+        self._send_message(data)
         return
     
     def on_train_end(self, logs):
-        self._data['text'] = '`An ongoing training is finished [ID: {}].`'.format(self._id)
-        self._data['parse_mode'] = 'Markdown'
-        self._id = None
-        self._send_message()
-        
+        data = {}
+        data['chat_id'] = self._chat_id
+        data['text'] = '`An ongoing training is finished.\n[TASK ID: {}]`'.format(self._task_id)
+        data['parse_mode'] = 'Markdown'
+        self._send_message(data)
+        self._plot()
         return
     
     def on_epoch_begin(self, epoch, logs):
+        metric = next(iter(self._monitor))
+        if self._monitor.get(metric) is None:
+            data = {}
+            data['chat_id'] = self._chat_id
+            data['text'] = '`I am monitoring {} of an ongoing training. I will post an update whenever this metric improves.\n[TASK ID: {}]`'.format(metric, self._task_id)
+            data['parse_mode'] = 'Markdown'
+            self._send_message(data)
         return
     
     def on_epoch_end(self, epoch, logs):
+        for metric in logs.keys():
+            self._history[metric].append(logs.get(metric))
+        metric = next(iter(self._monitor))
+        old_value = self._monitor.get(metric)
+        new_value = logs.get(metric)
+        if old_value is None:
+            self._monitor[metric] = new_value
+            self._checkpoint(epoch, metric, old_value, new_value)
+        elif metric.find('loss') >= 0 and new_value < old_value:
+            self._monitor[metric] = new_value
+            self._checkpoint(epoch, metric, old_value, new_value)
+        elif metric.find('acc') >= 0 and new_value > old_value:
+            self._monitor[metric] = new_value
+            self._checkpoint(epoch, metric, old_value, new_value)
         return
     
     def on_batch_begin(self, batch, logs):
@@ -57,11 +82,68 @@ class Telegram(callbacks.Callback):
     def on_batch_end(self, batch, logs):
         return
     
-    def _send_message(self):
-        requests.post(self._url+'/sendMessage', data=self._data)
+    def _checkpoint(self, epoch, metric, old_value, new_value):
+        if metric.find('loss') >= 0 and old_value is None:
+            old_value = '\u221e'
+        elif metric.find('acc') >= 0 and old_value is None:
+            old_value = '-\u221e'
+        data = {}
+        data['chat_id'] = self._chat_id
+        if type(old_value) is str:
+            data['text'] = '`{} of an ongoing training is improved from {} to {:.4f} after epoch {}.\n[TASK ID: {}]`'.format(metric, old_value, new_value, epoch+1, self._task_id)
+        else:
+            data['text'] = '`{} of an ongoing training is improved from {:.4f} to {:.4f} after epoch {}.\n[TASK ID: {}]`'.format(metric, old_value, new_value, epoch+1, self._task_id)
+        data['parse_mode'] = 'Markdown'
+        self._send_message(data)
         return
     
-    def _send_photo(self):
-        requests.post(self._url+'/sendPhoto', data=self._data,
-                      files=self._files)
+    def _plot(self):
+        if not os.path.isdir(self._out_dir):
+            os.makedirs(self._out_dir)
+        plot_l = os.path.join(self._out_dir, 'plot_loss.png')
+        plot_a = os.path.join(self._out_dir, 'plot_acc.png')
+        
+        # plot training and validation loss
+        pyplot.figure()
+        pyplot.title('Training and Validation Loss')
+        pyplot.xlabel('epoch')
+        pyplot.ylabel('loss')
+        for metric in self._history.keys():
+            if metric.find('loss') < 0:
+                continue
+            pyplot.plot(self._history[metric], label=metric)
+        pyplot.legend()
+        pyplot.savefig(plot_l)
+        
+        # plot training and validation accuracy
+        pyplot.figure()
+        pyplot.title('Training and Validation Accuracy')
+        pyplot.xlabel('epoch')
+        pyplot.ylabel('accuracy')
+        for metric in self._history.keys():
+            if metric.find('acc') < 0:
+                continue
+            pyplot.plot(self._history[metric], label=metric)
+        pyplot.legend()
+        pyplot.savefig(plot_a)
+        
+        # broadcast results
+        data = {}
+        data['chat_id'] = self._chat_id
+        data['caption'] = '`Training and Validation Loss [TASK ID: {}]`'.format(self._task_id)
+        data['parse_mode'] = 'Markdown'
+        files = {}
+        files['photo'] = open(plot_l, 'rb')
+        self._send_photo(data, files)
+        data['caption'] = '`Training and Validation Accuracy [TASK ID: {}]`'.format(self._task_id)
+        files['photo'] = open(plot_a, 'rb')
+        self._send_photo(data, files)
+        return
+    
+    def _send_message(self, data):
+        requests.post(self._address+'/sendMessage', data=data)
+        return
+    
+    def _send_photo(self, data, files):
+        requests.post(self._address+'/sendPhoto', data=data, files=files)
         return
